d32ea7ff43b02c9912bd8003da73c442b6853a2c
Leo Clinger <breeclinger@gmail.com>
2025-03-30 16:46:21 -0600

    Lab 10

---
+++ AdventureGameProject/Assets/Scripts/TriggerParticleEffect.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem), typeof(Collider))]
+public class TriggerParticleEffect : MonoBehaviour
+{
+    private ParticleSystem particleSystem;
+    public int particleAmount = 10;
+    void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<CharacterController>())
+        {
+            particleSystem.Emit(particleAmount);
+        }
+    }
+}
