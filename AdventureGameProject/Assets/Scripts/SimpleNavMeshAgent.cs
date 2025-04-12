4edc3eec8084f33a2b2ae975d42140c4531a6179
Leo Clinger <breeclinger@gmail.com>
2025-04-11 21:58:48 -0600

    Most of Lab 12, except NavMeshSurface not working

---
+++ AdventureGameProject/Assets/Scripts/SimpleNavMeshAgent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleNavMeshAgent : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+    }
+    
+    void Update()
+    {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+}
