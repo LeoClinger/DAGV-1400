5119a3b6d8da85fb3c66abdbc5517573f3c9e2dc
Bree Clinger <leocling3r@gmail.com>
2025-04-16 18:10:23 -0600

    added destroy script destroy collider before object

--- AdventureGameProject/Assets/Scripts/DestroyScript.cs
+++ AdventureGameProject/Assets/Scripts/DestroyScript.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class DestroyScript : MonoBehaviour
 {
+    private Collider myCollider;
+
+void Start()
+{
+    myCollider = GetComponent<Collider>();
+}
  
 public void Destoryer()
     {
+            Destroy(myCollider);
             Destroy(gameObject, 1f);
     }
 }
