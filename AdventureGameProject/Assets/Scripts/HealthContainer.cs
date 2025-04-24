60a7bc1173a8a22a0f5c90a71ac73e82d26cff3c
Leo Clinger <breeclinger@gmail.com>
2025-04-24 00:03:32 -0600

    Attempting to die and send to main menu

--- AdventureGameProject/Assets/Scripts/HealthContainer.cs
+++ AdventureGameProject/Assets/Scripts/HealthContainer.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HealthContainer : MonoBehaviour
 {
     public SimpleFloatData healthData;
+    public GameObject player;
 
     public void ReduceHealth(float amount)
     {
         healthData.UpdateValue(amount);
+    }
+
+    public void NoHealth(float amount)
+    {
+        if (healthData.value <= 0)
+        {
+            Destroy(player);
+            StartCoroutine(WaitForSceneLoad());
+        }
+    }
+    private IEnumerator WaitForSceneLoad()
+    {
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene("MainMenue");
     }
 }
